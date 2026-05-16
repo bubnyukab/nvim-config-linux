d79875150dfdd85beb2ae1008fa506e622670025
bubnyukab <bubnyukab@gmail.com>
2026-05-16 13:34:38 +0200

    Make vim-test cd to project root in vimux pane before running

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -6,7 +6,26 @@ return {
     "vim-test/vim-test",
     dependencies = { "preservim/vimux" },
     config = function()
-      vim.g["test#strategy"] = "vimux"
+      -- Resolve the project root for the current buffer (go.mod / Cargo.toml /
+      -- package.json / .git) and fall back to the buffer's own directory.
+      _G.__vim_test_cwd = function()
+        local path = vim.api.nvim_buf_get_name(0)
+        if path == "" then return vim.fn.getcwd() end
+        local root = vim.fs.root(path, { "go.mod", "Cargo.toml", "package.json", ".git" })
+        return root or vim.fn.fnamemodify(path, ":p:h")
+      end
+
+      -- Custom strategy: cd into the project root in the vimux pane before running.
+      -- This makes `:TestNearest` work no matter where nvim was launched from.
+      vim.cmd([[
+        function! TestVimuxCdStrategy(cmd) abort
+          let l:dir = luaeval('_G.__vim_test_cwd()')
+          call VimuxRunCommand('cd ' . shellescape(l:dir) . ' && ' . a:cmd)
+        endfunction
+        let g:test#custom_strategies = { 'vimux_cd': function('TestVimuxCdStrategy') }
+      ]])
+      vim.g["test#strategy"] = "vimux_cd"
+
       vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>", { desc = "Test nearest" })
       vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>",    { desc = "Test file" })
       vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<cr>",   { desc = "Test suite" })
